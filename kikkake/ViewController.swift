95b7c1ddeedb1fd9275dcd68b392efd5201bef59
Yuta Suzuki <kix@kixmate.net>
2019-11-30 23:14:12 +0900

    Initial Commit

---
+++ kikkake/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  kikkake
+//
+//  Created by 鈴木裕太 on 2019/11/30.
+//  Copyright © 2019 きっかけレシピ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
